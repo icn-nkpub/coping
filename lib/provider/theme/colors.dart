e0b8697acc9084e1bf755d990e412be5727884d3
Nikolai Kiselev <nkpub@icloud.com>
2023-07-16 16:33:44 +0200

    feat: add colors

--- lib/provider/theme/colors.dart
+++ lib/provider/theme/colors.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 
 enum ColorValue {
-  ocean(name: 'Ocean', canvas: Color(0xFF03A5FF), hueShift: -30),
-  peaches(name: 'Peaches', canvas: Color(0xFFFF3E45), hueShift: 30),
-  midnight(name: 'Midnight', canvas: Color(0xFF8A37FF), hueShift: -60),
-  slimes(name: 'Slimes', canvas: Color(0xFF70FFBB), hueShift: 60);
+  ocean(name: 'Ocean', canvas: Color(0xFF4bcffa)),
+  cherry(name: 'Cherry', canvas: Color(0xFFff5e57)),
+  rose(name: 'Rose', canvas: Color(0xFFef5777)),
+  forest(name: 'Forest', canvas: Color(0xFF575fcf)),
+  garden(name: 'Garden', canvas: Color(0xFF05c46b)),
+  sun(name: 'Sun', canvas: Color(0xFFffc048));
 
   const ColorValue({
     required this.name,
     required this.canvas,
-    required this.hueShift,
   });
 
   final String name;
   final Color canvas;
-  final double hueShift;
 
   Color get primary => HSLColor.fromColor(canvas).withLightness(.1).withSaturation(.1).toColor();
-  Color get special {
-    final c = HSLColor.fromColor(primary);
-    return c.withHue((360 + c.hue + hueShift) % 360).toColor();
-  }
 }
 
 ColorValue findThemeColor(String c) {
