0441d0571ee7fd1565bcd64e90f683a75fecfc9a
Nikolai Kiselev <nkpub@icloud.com>
2024-09-27 08:04:35 -0500

    feat: solana support

--- lib/pages/copingdao/copingdao.dart
+++ lib/pages/copingdao/copingdao.dart
@@ -1,3 +1,4 @@
+import 'package:bip39/bip39.dart' as bip39;
 import 'package:dependencecoping/tokens/topbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -15,57 +16,16 @@ class CopeScreen extends StatelessWidget {
     final sc = SolanaClient(
         rpcUrl: Uri.parse('http://127.0.0.1:8899'),
         websocketUrl: Uri.parse('ws://127.0.0.1:8900'));
-    print(sc);
 
-    late final Wallet w;
-    late final double balance;
-    try {
-      final sbt = [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26,
-        27,
-        28,
-        29,
-        30,
-        31,
-        32
-      ];
+    var m = bip39.generateMnemonic();
+    print(m);
+    m = 'crystal scout antique seek shell engage comfort bounce win rally napkin juice';
 
-      // Finally, create a new wallet
-      w = await Ed25519HDKeyPair.fromSeedWithHdPath(
-        seed: sbt,
-        hdPath: "m/44'/501'/0'/0'",
-      );
-      print(w.address);
-      final bal = await sc.rpcClient.getBalance(w.publicKey.toString());
-      balance = bal.value.toDouble() / 1000000000;
-    } catch (e) {
-      print(e);
-    }
+    // Finally, create a new wallet
+    final w = await Ed25519HDKeyPair.fromMnemonic(m);
+    print(w.address);
+    final bal = await sc.rpcClient.getBalance(w.publicKey.toString());
+    final balance = bal.value.toDouble() / 1000000000;
 
     return '${w.address.substring(0, 4)}...${w.address.substring(w.address.length - 5, w.address.length - 1)}, $balance SOL';
   }
@@ -81,10 +41,13 @@ class CopeScreen extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  // ignore: discarded_futures
                   FutureBuilder(
+                      // ignore: discarded_futures
                       future: wall(),
                       builder: (final context, final snapshot) {
+                        if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        }
                         if (!snapshot.hasData) {
                           return const Text('...');
                         }
